481a48cda7ada7eb96acd4831847ce43fa20b97b
Bakhtiyorjon Mirzajonov <bakhtiyorjon.m@gmail.com>
2022-06-26 00:00:57 +0300

    completed the functions

--- class16/class16-materials/simple-functions/js/main.js
+++ class16/class16-materials/simple-functions/js/main.js
@@ -26,3 +26,13 @@ console.log(arithmetic(6,2,5))
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+function hard(n1,n2,n3,n4) {
+    let product = n1*n2
+    if (product > 100) {
+        console.log(product+n3+n4)
+    } else if (product < 100) {
+        console.log(product-n3-n4)
+    } else {
+        alert((n1*n2*n3)%n4)
+    }
+}
