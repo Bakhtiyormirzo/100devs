f6e82b772d4086428315f8ce26d930957fdf376f
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-11-07 19:11:55 +0300

    objects

--- class20/class20-materials /objects-listen-to-your-heart/js/main.js
+++ class20/class20-materials /objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,22 @@
 //Create a stopwatch object that has four properties and three methods
+const stopwatch = {
+    currentTime : 0,
+    started : false,
+    reset : false,
+    counting : 0,
+
+    resetter: function() {
+        this.currentTime = 0
+        this.started = false
+        reset = true
+    },
+
+    counter: function() {
+        this.counting++;
+    },
+
+    end: function() {
+        this.currentTime = 0
+        this.counting = 0
+    }
+}
