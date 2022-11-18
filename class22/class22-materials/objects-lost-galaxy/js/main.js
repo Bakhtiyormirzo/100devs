48605a99934b3da04bc8f73fab81436335d494e8
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-11-18 15:51:27 +0300

    objects

--- class22/class22-materials/objects-lost-galaxy/js/main.js
+++ class22/class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,16 @@
 //Create a mouse object that has four properties and three methods
+const Mouse = {}
+Mouse.color = 'black'
+Mouse.brand = 'Logitec'
+Mouse.model = 'MX Ergo'
+Mouse.wireless = true 
+
+Mouse.leftClick = function() {
+    console.log('Left Click')
+}
+Mouse.rightClick = function() {
+    console.log('Right Click')
+}
+Mouse.scroll = function() {
+    console.log('Jump Jump Jump')
+}
