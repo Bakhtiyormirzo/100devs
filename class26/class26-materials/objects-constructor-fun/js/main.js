518a3278deead56e037ff3595b32349b2134412b
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-11-30 11:31:58 +0300

    fetch api

--- class26/class26-materials/objects-constructor-fun/js/main.js
+++ class26/class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,22 @@
 //Create a constructor with 4 properties and 3 methods
+
+function MakePizza(size, toppings, crust, sause) {
+    this.size = size
+    this.toppings = toppings
+    this.crust = crust
+    this.sause = sause
+
+    this.estimatedTimeDelivery = function() {
+        console.log('Calculating ...')
+    }
+
+    this.burnMouth = function() {
+        console.log('VVjbbqbxwuxqwx')
+    }
+    
+    this.frisbee = function() {
+        console.log('Yeeeeet')
+    }
+}
+
+let pizza = new MakePizza('large', ['spinage', 'garlic'], 'deep fish', 'white')
