0ef0b48f7fd8015bfc54b1d6f5602a5398d55854
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2022-11-30 10:23:00 +0300

    class 26 materials

---
+++ class26/class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,13 @@
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.size = 'large'
+pizza.crust = 'stuffed'
+pizza.toppings = ['spinage', 'onion', 'jalapenos']
+pizza.sause = 'heavy'
+
+pizza.taste = function() {
+    console.log('Tastes awesome')
+}
+
